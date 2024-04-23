4f8bf643933fda0c3d623e35bfa8e99214cbae40
Jake Howard <jake.howard@torchbox.com>
2024-04-23 10:40:00 +0100

    Set allowed hosts

--- wagtail_starter/settings/base.py
+++ wagtail_starter/settings/base.py
@@ -18,6 +18,8 @@ import dj_database_url
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
